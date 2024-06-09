5d0825a510169edc17d0fa09be8776cff29dfe21
imnaiyar <imamnaiyar29@gmail.com>
2024-06-10 00:32:38 +0530

    endpoint change

--- api/config.ts
+++ api/config.ts
@@ -1,5 +1,5 @@
 export default {
     PORT: 3000,
     CLIENT_ID: "1121541967730450574",
-    API_ENDPOINT: "https://localhost:8080" // endpoint for getting the bot stats
+    API_ENDPOINT: "https://api.skyhelper.xyz" // endpoint for getting the bot stats
 };
